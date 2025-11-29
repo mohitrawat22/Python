f748ca23edf1934f72ed30b7119a6ada87dfea15
mohitrawat22 <mohitrawat22@gmail.com>
2025-11-29 19:25:54 +0530

    input functions

--- Python-1/todo_app/main.py
+++ Python-1/todo_app/main.py
@@ -1,1 +1,15 @@
-print("Enter a todo: ")
+# print("Enter a todo: ")
+# user_input = input()
+# print(user_input)
+
+# user_input = input("Enter a todo: ")
+# print(user_input)
+
+user_prompt = "Enter a todo: "
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(todos))
