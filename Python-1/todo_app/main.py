a1a570acf9e4a892cf416d949025f763c0e29de1
mohitrawat22 <mohitrawat22@gmail.com>
2025-12-10 20:57:36 +0530

    multiple user input in a list

--- Python-1/todo_app/main.py
+++ Python-1/todo_app/main.py
@@ -1,3 +1,4 @@
+# user input
 # print("Enter a todo: ")
 # user_input = input()
 # print(user_input)
@@ -5,11 +6,29 @@
 # user_input = input("Enter a todo: ")
 # print(user_input)
 
-user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+# multiple user input
+# user_prompt = "Enter a todo: "
+# todo1 = input(user_prompt)
+# todo2 = input(user_prompt)
+# todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
-print(type(todos))
+# todos = [todo1, todo2, todo3]
+# print(todos)
+# print(type(todos))
+
+# # multiple user input in a loop
+# user_prompt = "Enter a todo: "
+# while True:
+#     todo = input(user_prompt)
+#     print(todo)
+#     print("Next ...")
+
+# multiple user input in a list
+user_prompt = "Enter a todo: "
+todos = []
+while True:
+    todo = input(user_prompt)
+    todos.append(todo)
+    print(todos)
+    print("Next ...")
+
